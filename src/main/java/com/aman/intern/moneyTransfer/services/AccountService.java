264f4911850cc91318fc25c78a9c8b43695eee26
othman <othman.nasser274@gmail.com>
2026-08-15 22:15:34 +0300

    solve warnings and remove unuseful imports

--- src/main/java/com/aman/intern/moneyTransfer/services/AccountService.java
+++ src/main/java/com/aman/intern/moneyTransfer/services/AccountService.java
@@ -3,18 +3,12 @@ package com.aman.intern.moneyTransfer.services;
 import com.aman.intern.moneyTransfer.models.DTO.BalanceResponseDTO;
 import com.aman.intern.moneyTransfer.models.entities.Account;
 import com.aman.intern.moneyTransfer.models.entities.User;
-import com.aman.intern.moneyTransfer.reposatories.AccountRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class AccountService {
-    private final AccountRepository ACCOUNTREPOSITORY;
-
-    public AccountService(AccountRepository accountRepository) {
-        this.ACCOUNTREPOSITORY = accountRepository;
-    }
 
     public BalanceResponseDTO getBalance(User user) {
 //         get user account using get method
